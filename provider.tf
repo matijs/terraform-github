cc56dc6bbd659f0ab920a6e358d560d5d268a1a4
matijs <matijs@gmail.com>
2024-07-26 11:50:15 +0200

    refactor: set GITHUB_TOKEN using Terraform var
    Use a Terraform input variable to provide GITHUB_TOKEN instead of
    relying on an environment variable.

--- provider.tf
+++ provider.tf
@@ -14,3 +14,7 @@ terraform {
     }
   }
 }
+
+provider "github" {
+  token = var.GITHUB_TOKEN
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "GITHUB_TOKEN" {
+  description = "GitHub personal access token"
+  type        = string
+  sensitive   = true
+}
+
