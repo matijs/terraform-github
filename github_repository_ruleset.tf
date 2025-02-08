94d2912d4a9b4b235fb9448fd6bca4483d5d5eee
matijs <matijs@gmail.com>
2025-02-08 13:54:15 +0100

    fix: remove github_repository_ruleset
    It seems "repository" is not optional at all so this failed.

--- github_repository_ruleset.tf
+++
@@ -1,9 +0,0 @@
-resource "github_repository_ruleset" "require-linear-history" {
-  enforcement = "active"
-  name        = "require-linear-history"
-  target      = "branch"
-
-  rules {
-    required_linear_history = true
-  }
-}
