48d601e432097425db18e690eb67d06f4394454d
matijs <matijs@gmail.com>
2025-03-30 13:57:38 +0200

    fix: state

--- provider.tf
+++ provider.tf
@@ -8,13 +8,6 @@ terraform {
       name = "github"
     }
   }
-
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "6.6.0"
-    }
-  }
 }
 
 provider "github" {
