749dc2d368d15242fc9463704c684475a40ef2d2
matijs <matijs@gmail.com>
2025-03-30 16:55:21 +0200

    fix: everything

--- repositories/terraform-github.tf
+++ repositories/terraform-github.tf
@@ -1,7 +1,7 @@
-module "github-terraform" {
+module "terraform-github" {
   source = "../modules/repository"
 
-  name        = "github-terraform"
+  name        = "terraform-github"
   description = "Create GitHub repositories using Terraform"
   topics = [
     "terraform",
@@ -13,10 +13,10 @@ module "github-terraform" {
       context        = "Terraform format check"
       integration_id = 15368
     },
-    {
-      context        = "Terraform Cloud/matijs/repo-id-xTdtApDiyNBVUu6g"
-      integration_id = 39328
-    },
+    #{
+    #context        = "Terraform Cloud/matijs/repo-id-xTdtApDiyNBVUu6g"
+    #integration_id = 39328
+    #},
     {
       context        = "no-fixups"
       integration_id = 15368
