d10515a3f666bf027dc0237c1e3321c161f04507
matijs <matijs@gmail.com>
2025-03-30 19:13:06 +0200

    fix: lab.probablerobot.net required checks
    Update the required checks for lab.probablerobot.net

--- lab-probablerobot-net.tf
+++ lab-probablerobot-net.tf
@@ -8,7 +8,24 @@ module "lab-probablerobot-net" {
   # github_repository_ruleset required_status_checks
   required_checks = [
     {
-      context = "no-fixups"
+      context        = "Header rules"
+      integration_id = 13473
     },
+    {
+      context        = "Pages changed"
+      integration_id = 13473
+    },
+    {
+      context        = "Redirect rules"
+      integration_id = 13473
+    },
+    {
+      context        = "lint"
+      integration_id = 15368
+    },
+    {
+      context        = "no-fixups"
+      integration_id = 15368
+    }
   ]
 }
