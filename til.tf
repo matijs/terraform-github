2196210a40069aa33372407201d9821e495349f9
matijs <matijs@gmail.com>
2025-03-30 19:43:02 +0200

    feat(til): add no-fixups status check

--- til.tf
+++ til.tf
@@ -8,6 +8,10 @@ module "til" {
     {
       context        = "prettier-check",
       integration_id = 15368
+    },
+    {
+      context        = "no-fixups"
+      integration_id = 15368
     }
   ]
 }
