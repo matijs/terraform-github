c42c95dcea503fc059b5e007321145100b5b7fbd
matijs <matijs@gmail.com>
2025-03-31 07:01:52 +0200

    fix: remove Netlify checks
    Disable the Netlify checks to see if this will allow Dependabot to merge
    its pull requests again using `@dependabot merge`.

--- lab-probablerobot-net.tf
+++ lab-probablerobot-net.tf
@@ -7,18 +7,18 @@ module "lab-probablerobot-net" {
 
   # github_repository_ruleset required_status_checks
   required_checks = [
-    {
-      context        = "Header rules"
-      integration_id = 13473
-    },
-    {
-      context        = "Pages changed"
-      integration_id = 13473
-    },
-    {
-      context        = "Redirect rules"
-      integration_id = 13473
-    },
+    #{
+    #context        = "Header rules"
+    #integration_id = 13473
+    #},
+    #{
+    #context        = "Pages changed"
+    #integration_id = 13473
+    #},
+    #{
+    #context        = "Redirect rules"
+    #integration_id = 13473
+    #},
     {
       context        = "lint"
       integration_id = 15368
--- probablerobot-net.tf
+++ probablerobot-net.tf
@@ -7,18 +7,18 @@ module "probablerobot-net" {
 
   # github_repository_ruleset required_status_checks
   required_checks = [
-    {
-      context        = "Header rules"
-      integration_id = 13473
-    },
-    {
-      context        = "Pages changed"
-      integration_id = 13473
-    },
-    {
-      context        = "Redirect rules"
-      integration_id = 13473
-    },
+    #{
+    #  context        = "Header rules"
+    #  integration_id = 13473
+    #},
+    #{
+    #  context        = "Pages changed"
+    #  integration_id = 13473
+    #},
+    #{
+    #  context        = "Redirect rules"
+    #  integration_id = 13473
+    #},
     {
       context        = "check"
       integration_id = 15368
