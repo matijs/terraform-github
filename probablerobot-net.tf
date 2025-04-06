1a37b21aefac436cb9dc48e4ff8d7966a39d3d51
matijs <matijs@gmail.com>
2025-04-06 16:52:41 +0200

    chore: enable CodeQL check for probablerobot.net

--- probablerobot-net.tf
+++ probablerobot-net.tf
@@ -7,25 +7,17 @@ module "probablerobot-net" {
 
   # github_repository_ruleset required_status_checks
   required_checks = [
-    #{
-    #  context        = "Header rules"
-    #  integration_id = 13473
-    #},
-    #{
-    #  context        = "Pages changed"
-    #  integration_id = 13473
-    #},
-    #{
-    #  context        = "Redirect rules"
-    #  integration_id = 13473
-    #},
     {
-      context        = "check"
+      context        = "lint"
       integration_id = 15368
     },
     {
       context        = "no-fixups"
       integration_id = 15368
+    },
+    {
+      context        = "CodeQL"
+      integration_id = 57789
     }
   ]
 }
