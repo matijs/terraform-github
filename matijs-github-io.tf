6c5142098f3bca947dff5f04d0076f0060bcdaa5
matijs <matijs@gmail.com>
2025-04-13 21:36:48 +0200

    chore: improve matijs-github-io
    Add description and homepage_url

--- matijs-github-io.tf
+++ matijs-github-io.tf
@@ -1,7 +1,9 @@
 module "matijs-github-io" {
   source = "./modules/repository"
 
-  name = "matijs.github.io"
+  name         = "matijs.github.io"
+  description  = "Source code for https://matijs.github.io"
+  homepage_url = "https://matijs.github.io"
 
   pages = {
     build_type = "legacy"
