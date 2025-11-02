d4c70743a7d50427d0771a839cd1450d8cfc887a
matijs <matijs@gmail.com>
2025-11-02 21:57:51 +0000

    chore: tighten required checks
    Make the no-fixups and terraform format check mandatory for the
    terraform-netlify repo

--- terraform-netlify.tf
+++ terraform-netlify.tf
@@ -10,8 +10,16 @@ module "terraform-netlify" {
 
   required_checks = [
     {
+      context        = "Terraform format check"
+      integration_id = 15368
+    },
+    {
       context        = "Terraform Cloud/matijs/repo-id-ryohofGxm6tLFpKk"
       integration_id = 39328
+    },
+    {
+      context        = "no-fixups"
+      integration_id = 15368
     }
   ]
 }
