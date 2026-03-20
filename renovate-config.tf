8eef0684fae9e0d60964cce156eb2829273f1406
matijs <matijs@gmail.com>
2026-03-20 08:55:30 +0100

    chore: update renovate-config
    Add the no-fixups check. This also enforces branches to be up to date
    from now on.

--- renovate-config.tf
+++ renovate-config.tf
@@ -3,4 +3,11 @@ module "renovate-config" {
 
   name        = "renovate-config"
   description = "Renovate configuration"
+
+  required_checks = [
+    {
+      context        = "no-fixups"
+      integration_id = 15368
+    }
+  ]
 }
