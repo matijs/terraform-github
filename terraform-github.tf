2524c252b0cc997ff2658f61cd8f3fe7a3469da9
matijs <matijs@gmail.com>
2026-05-23 09:30:02 +0200

    feat: add zizmor to terraform-github
    Add the zizmorecore/zizmore-action check as a required check on the
    terraform-github repository (this repository).

--- terraform-github.tf
+++ terraform-github.tf
@@ -11,15 +11,19 @@ module "terraform-github" {
   required_checks = [
     {
       context        = "Terraform format check"
-      integration_id = 15368
+      integration_id = 15368 # GitHub Actions
     },
     {
       context        = "Terraform Cloud/matijs/repo-id-G6r89aYrVaCsUhv5"
-      integration_id = 39328
+      integration_id = 39328 # Terraform Cloud
     },
     {
       context        = "no-fixups"
-      integration_id = 15368
+      integration_id = 15368 # GitHub Actions
+    },
+    {
+      context        = "zizmor"
+      integration_id = 57789 # GitHub Advanced Security
     }
   ]
 }
